d9965448156510553aa9ea05861bd8bfbd18d3f8
Debayan2004 <debayandasgupta12012004@gmail.com>
2025-07-31 11:11:19 +0530

    requirements added successfully

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='MedicalChatbot',
+    version='0.1.0',
+    author='Debayan Das Gupta',
+    author_email='debayanofficial123@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
